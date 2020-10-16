15b801f54187d99b80f2b12f19e342e7680a711d
ale2157 <ale2157334@maricopa.edu>
2020-10-16 02:48:06 -0700

    Initial commit

---
+++ src/movieapp/FXMLDocumentController.java
@@ -0,0 +1,91 @@
+package movieapp;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+/**
+ *
+ * @author Alexandra
+ */
+public class FXMLDocumentController implements Initializable {
+    // config table
+    @FXML private TableView<Movie> tableView;
+    @FXML private TableColumn<Movie, String> movieNameColumn;
+    @FXML private TableColumn<Movie, String> shelfNumberColumn;
+    // search box
+    @FXML private final Label searchLabel = new Label();
+    @FXML private TextField searchText = new TextField();
+    // Observable List to store data
+    private final ObservableList<Movie> movieData = FXCollections.observableArrayList();
+    
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        //sets up the columns in the table
+        movieNameColumn.setCellValueFactory(new PropertyValueFactory<>("movieName"));
+        shelfNumberColumn.setCellValueFactory(new PropertyValueFactory<>("shelfNumber"));
+        // load data
+        getMovieFromFile();
+        // search for movie from loaded data
+        searchBox();         
+    }    
+    
+    // This method will get list of movies from file, store in Movie object, and add to our ObservableList
+    public Movie getMovieFromFile() {
+        Movie movie = null;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(new File("TestFile.txt")));
+            String line;
+            String[] array;
+            while((line = br.readLine()) != null) {
+                array = line.split("#");            
+                movie = new Movie((array[0]), (array[1]));
+                movieData.addAll(movie);
+            }
+            br.close();
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+        return movie;
+    }
+    
+    // This method will predict search as you type and allow you to find a specific movie
+    public void searchBox() {
+        FilteredList<Movie> filteredData = new FilteredList<>(movieData, p -> true);
+        searchText.textProperty().addListener((observable, oldValue, newValue) -> {
+            filteredData.setPredicate(movie -> {
+               if(newValue == null || newValue.isEmpty()) {
+                   return true;
+               }
+                if(movie.getMovieName().toLowerCase().contains(newValue.toLowerCase())) {
+                    return true;
+                } else if(movie.getShelfNumber().toLowerCase().contains(newValue.toLowerCase())) {
+                    return true;
+                } else {
+                    return false;
+               }
+            });
+        });
+        SortedList<Movie> sortedData = new SortedList<>(filteredData);
+        sortedData.comparatorProperty().bind(tableView.comparatorProperty());
+        tableView.setItems(sortedData);
+    }
+             
+    
+}
---
+++ src/movieapp/Movie.java
@@ -0,0 +1,33 @@
+package movieapp;
+
+import javafx.beans.property.SimpleStringProperty;
+
+/**
+ *
+ * @author Alexandra
+ */
+public class Movie {
+    private SimpleStringProperty movieName;
+    private SimpleStringProperty shelfNumber;
+    
+    public Movie(String movieName, String shelfNumber) {
+        this.movieName = new SimpleStringProperty(movieName);
+        this.shelfNumber = new SimpleStringProperty(shelfNumber);
+    }
+    
+    public String getMovieName() {
+        return movieName.get();
+    }
+    
+    public void setMovieName(SimpleStringProperty movieName) {
+        this.movieName = movieName;
+    }
+    
+    public String getShelfNumber() {
+        return shelfNumber.get();
+    }
+    
+    public void setShelfNumber(SimpleStringProperty shelfNumber) {
+        this.shelfNumber = shelfNumber;
+    }
+}
